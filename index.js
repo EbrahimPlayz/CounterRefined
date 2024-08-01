b928b28b333075350d768d7f73a6a435cf2bcf31
EbrahimPlayz <ebrahimshoaibis12@gmail.com>
2024-08-01 10:39:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const decreasebutton = document.getElementById("decreasebutton")
+const resetbutton = document.getElementById("resetbutton")
+const increasebutton = document.getElementById("increasebutton")
+const countlabel = document.getElementById("countlabel")
+let count = 0;
+
+increasebutton.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+
+decreasebutton.onclick = function(){
+    count = count - 1;
+    countlabel.textContent = count;
+}
+
+resetbutton.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
